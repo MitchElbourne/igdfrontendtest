a549e217c222ef0e97fd04793d667897854df348
MitchElbourne <mkelbourne@gmail.com>
2020-07-02 15:33:48 +0100

    Added: Image, Name and description to modal

--- src/assets/components/Card.vue
+++ src/assets/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="card">
-        <img v-bind:src="product.images.main" v-bind:alt="product.name" class="card-img-top">
+        <img v-bind:src="product.images.thumb" v-bind:alt="product.name" class="card-img-top">
         <div class="card-body">
 
             <h5 class="card-title">{{product.name}}</h5>
@@ -10,6 +10,20 @@
 
                 <b-modal v-bind:ref="'modal-' + product.id" hide-footer hide-header class="modal">
                     <b-button id='close-btn' @click="hideModal"><img src="../close.svg" alt="close-icon"></b-button>
+                    
+                    <div class="panel-wrapper">    
+                        <div class="left-panel">
+                            <img v-bind:src="product.images.main" v-bind:alt="product.name" class="img-fluid">
+                        </div>
+                        <div class="right-panel">
+                            <h2 class="product-name">{{product.name}}</h2>
+                            <p>{{product.description}}</p>
+                            <b>Enter Measurements to get a price:</b>
+
+                        </div>
+                    </div>
+
+
                 </b-modal>
             </div>
         </div>
